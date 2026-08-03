16c4fc9053abbe75212f622d5d816ff43ee16f40
MUNAWAR-dot <118822053+MUNAWAR-dot@users.noreply.github.com>
2026-08-04 03:28:23 +0500

    Fix import for ChatsScreen in TabNavigator

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,6 +1,7 @@
+// src/navigation/TabNavigator.js
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ChatListScreen from '../screens/Chats/ChatListScreen';
+import ChatsScreen from '../screens/Chats/ChatListScreen';
 import StatusScreen from '../screens/Status/StatusScreen';
 import CallsScreen from '../screens/Calls/CallsScreen';
 import SettingsScreen from '../screens/Settings/SettingsScreen';
@@ -10,7 +11,7 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Chats" component={ChatListScreen} />
+      <Tab.Screen name="Chats" component={ChatsScreen} />
       <Tab.Screen name="Status" component={StatusScreen} />
       <Tab.Screen name="Calls" component={CallsScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
