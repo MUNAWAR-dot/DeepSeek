21324eb9278c7e69be69b8d48c0fed71ce887252
MUNAWAR-dot <118822053+MUNAWAR-dot@users.noreply.github.com>
2026-08-04 02:40:16 +0500

    Change Babel preset and remove module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,6 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['babel-preset-expo'],
   plugins: [
     'react-native-reanimated/plugin',
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@': './src',
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@navigation': './src/navigation',
-          '@services': './src/services',
-          '@store': './src/store',
-          '@utils': './src/utils',
-          '@hooks': './src/hooks',
-          '@assets': './src/assets',
-          '@config': './src/config',
-          '@i18n': './src/i18n',
-        },
-      },
-    ],
   ],
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
-    },
-  },
 };
