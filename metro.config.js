cb6054c1dcbe33e90c5dc3098c0956d1411895aa
MUNAWAR-dot <118822053+MUNAWAR-dot@users.noreply.github.com>
2026-08-04 02:38:54 +0500

    Add web support to Metro configuration
    Added web support to the Metro configuration.

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add web support
+config.resolver.platforms = ['ios', 'android', 'web'];
+
 module.exports = config;
