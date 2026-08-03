3b24f8ec8031b26497c0580ce3f53fa8bf6e03f0
MUNAWAR-dot <118822053+MUNAWAR-dot@users.noreply.github.com>
2026-08-03 23:04:14 +0500

    Refactor metro.config.js to use default config
    Simplified metro.config.js by removing custom transformer and resolver settings.

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
-
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
-    extraNodeModules: {
-      crypto: require.resolve('crypto-js'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer'),
-    },
-  },
-};
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
