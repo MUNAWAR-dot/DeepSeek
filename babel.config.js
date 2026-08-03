198a908f27c063519bdca3ab2eedda30237b5753
MUNAWAR-dot <118822053+MUNAWAR-dot@users.noreply.github.com>
2026-08-03 12:27:33 +0500

    Add Babel configuration for React Native project

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@assets': './src/assets',
+          '@config': './src/config',
+          '@i18n': './src/i18n',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+};
